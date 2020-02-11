a5e6aa7721c564f75586e556da081d30b48af9fc
Mauro Ali Gomez <maurogo@lagash.com>
2020-02-11 18:40:15 -0300

    Commit interfaces

---
+++ Tokoyami.Bot/Tokoyami.Business/Business/HangmanService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tokoyami.Business.Contract;
+
+namespace Tokoyami.Business.Business
+{
+    public class HangmanService : IHangmanService
+    {
+    }
+}
---
+++ Tokoyami.Bot/Tokoyami.Business/Contract/IHangmanService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tokoyami.Business.Contract
+{
+    public interface IHangmanService
+    {
+    }
+}
