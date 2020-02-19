ef78eb2e95b2f638338840540d719cf6d8886705
Mauro Ali Gomez <maurogo@lagash.com>
2020-02-19 15:17:29 -0300

    Create Word entity

---
+++ Tokoyami.Bot/Tokoyami.EF/Hangman/Entities/Word.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tokoyami.EF.Hangman.Entities
+{
+    [Table("Word", Schema = "hangman")]
+    public class Word
+    {
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
